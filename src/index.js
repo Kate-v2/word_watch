b07240c5bd38c647a9d8d970c36d7acd99425c4d
Katelyn <katelyn.rimkunas@gmail.com>
2019-02-11 00:54:44 -0700

    Posting each word is functional

--- src/index.js
+++ src/index.js
@@ -20,11 +20,13 @@ function showTopWord(obj) {
 
 function showWord(word) {
   let section       = document.getElementById('topWord')
+  section.innerHTML = ''
   section.innerHTML = titlize(word)
 }
 
 function showCount(count) {
   let section       = document.getElementById('topWordCount')
+  section.innerHTML = ''
   section.innerHTML = `Count: ${count}`
 }
 
@@ -32,12 +34,63 @@ function titlize(word) {
   return word.charAt(0).toUpperCase() + word.slice(1)
 }
 
+function addWordsSubmission() {
+  let button     = document.getElementById('addWords')
+  button.onclick = function() { postWords() }
+}
+
+function postWords() {
+  let words = getWords()
+  for (let i=0; i < words.length; i++) {
+    // TO DO - add letter validation
+    // if not letter, send word into an array
+    // print the array to user as words not added.
+    // - OR - If word had non-letter, remove it and post word?
+    postWord(words[i])
+  }
+}
+
+function postWord(word){
+  let url  = 'https://wordwatch-api.herokuapp.com/api/v1/words'
+  let json = { 'word': { 'value': word } }
+  debugger
+  fetch(url, {
+    method:  'POST',
+    headers: { 'Content-type': 'application/json' },
+    body:    JSON.stringify(json)
+  })
+    .then(response => response.json())
+    .then(msg => assessPost(msg, word) )
+    .catch(error => assessPost(msg, word))
+}
+
+let postCount = 0
+let notPosted = []
+// // Don't forget to clear this between submissions
+function assessPost(message, word) {
+  let msg = message.message
+  if ( !msg.endsWith('added!') ) { notPosted.push(word) }
+  postCount += 1
+  getTopWord()
+}
+// I'm not sure how to display these after all of the posts are attempted
+// TO DO - display notPosted as unordered-list
+
+
+
+function getWords() {
+  let input = document.getElementById('words').value
+  // - This is limited, if multiple spaces are added,
+  // then empty strings are returned in the array.
+  // - Also does not handle characters other than letters
+  return input.split(' ')
+}
+
+
+
+
 
 $(document).ready(() => {
-
   getTopWord()
-
-
-
-
+  addWordsSubmission()
 })
